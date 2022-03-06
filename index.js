236b461ab0994f641ddbfef08c7951c6c37112e4
Joao Guimaraes <99269515+Joaopsguimaraes@users.noreply.github.com>
2022-03-06 16:29:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { Produto } from "./produto.js"
 import { Fabricante } from "./fabricante.js"
 
-const produto1 = new Produto(1, 'Notebook', '1000.00', '1500.00')
+const produto1 = new Produto(1, 'Notebook', 1000, 1500)
 const fabricante1 = new Fabricante('Distridor X', '00.000.000/0001-00')
 
-produto1.novoCusto('1200.00')
+produto1.novoCusto(1200)
 produto1.vendaMargem(50)
 
 console.log(`Seu ${produto1.descricaoItem} tem o custo de R$ ${produto1.custo} e será vendido a R$${produto1.venda}`)
